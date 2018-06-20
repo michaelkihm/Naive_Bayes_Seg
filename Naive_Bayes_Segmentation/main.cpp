154fa1ecf3a86e301b05a139ed524a49ea873714
michaelkihm <michael.kihm@web.de>
2018-06-20 23:37:20 +0200

    Init openCV library

--- Naive_Bayes_Segmentation/main.cpp
+++ Naive_Bayes_Segmentation/main.cpp
@@ -7,9 +7,10 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    std::cout << CV_VERSION << std::endl;;
     return 0;
 }
