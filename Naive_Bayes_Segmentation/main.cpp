8748ae627227c50b70b6a690d8371a8f44f6bf04
michaelkihm <michael.kihm@web.de>
2018-06-20 23:20:56 +0200

    Initial Commit

---
+++ Naive_Bayes_Segmentation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Naive_Bayes_Segmentation
+//
+//  Created by Michael Kihm on 20.06.18.
+//  Copyright © 2018 MK. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
